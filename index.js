81cd0e4d17394e6dc271bb1cc084dceb9534097d
David Valencia <dvalencia@drift.com>
2022-09-02 14:06:15 -0400

    Add functionality to query leads before adding and not adding lead if already exists

--- index.js
+++ index.js
@@ -39,13 +39,9 @@ app.post('/endpoint', async (req, res) => {
     const contactId = conversationReturned.contactId
     const contactReturned = await getDriftApi(contactsApiBase, contactId)
     const conversationTranscript = await getTranscript(conversationId)
-    console.log('===TRANSCRIPT===')
-    console.log(conversationTranscript)
     const contactEmail = contactReturned.attributes.email
     const contactFirstName = contactReturned.attributes.first_name
     const contactLastName = contactReturned.attributes.last_name
-    //console.log('===contact===')
-    //console.log(contactReturned)
     createMondayItem(contactFirstName, contactLastName, contactEmail, conversationTranscript)
 })
 
@@ -74,24 +70,46 @@ function getTranscript(conversationId) {
 }
 
 function createMondayItem(contactFirstName, contactLastName, contactEmail, conversationTranscript) {
-    let yourDate = new Date()
-console.log(yourDate.toISOString().split('T')[0])
-    const variables = ({
-        boardId: 3169358815,
-        itemName: `${contactFirstName} ${contactLastName}`,
-        columnValues: JSON.stringify({
-            email: { email: `${contactEmail}`, "text": `${contactEmail}` },
-            text: "Drift",
-            status: { "label": "New" },
-            date4: { "date": new Date().toISOString().slice(0, 10)}
-        })
-    });
+    //Checking if Lead already exists
 
     axios({
         url: 'https://api.monday.com/v2',
         method: 'post',
         data: {
-            query: `mutation create_item ($boardId: Int!, $itemName: String!, $columnValues: JSON!) { 
+            query: `query {
+    items_by_column_values (board_id: 3169358815, column_id: "email", column_value: ${JSON.stringify(contactEmail)}) {
+        id
+        name
+    }
+}`,
+        },
+        headers: {
+            'Authorization': MONDAY_AUTH_TOKEN,
+            'Content-Type': 'application/json'
+        }
+    })
+        .then(res => {
+            console.log('Query Leads', res.data.data.items_by_column_values)
+            if (res.data.data.items_by_column_values.length > 0) {
+                console.log(`A lead with the email ${contactEmail} already exists. The name is ${res.data.data.items_by_column_values[0].name}`)
+            } else {
+                // Create new Item in Leads Board
+                const variables = ({
+                    boardId: 3169358815,
+                    itemName: `${contactFirstName} ${contactLastName}`,
+                    columnValues: JSON.stringify({
+                        email: { email: `${contactEmail}`, "text": `${contactEmail}` },
+                        text: "Drift",
+                        status: { "label": "New" },
+                        date4: { "date": new Date().toISOString().slice(0, 10) }
+                    })
+                });
+
+                axios({
+                    url: 'https://api.monday.com/v2',
+                    method: 'post',
+                    data: {
+                        query: `mutation create_item ($boardId: Int!, $itemName: String!, $columnValues: JSON!) { 
                 create_item (
                     board_id: $boardId,
                     item_name: $itemName, 
@@ -101,44 +119,42 @@ console.log(yourDate.toISOString().split('T')[0])
                     id
                 } 
             }`,
-            variables
-        },
-        headers: {
-            'Authorization': MONDAY_AUTH_TOKEN,
-            'Content-Type': 'application/json'
-        }
-    })
-        .then((result) => {
-            console.log('new item id', result.data.data.create_item.id)
-            const newItemId = result.data.data.create_item.id
-            const updateVariables = ({
-                item_id: newItemId
-            });
-
-            axios({
-                url: 'https://api.monday.com/v2',
-                method: 'post',
-                data: {
-                    query: `mutation{
+                        variables
+                    },
+                    headers: {
+                        'Authorization': MONDAY_AUTH_TOKEN,
+                        'Content-Type': 'application/json'
+                    }
+                })
+                    .then((result) => {
+                        //Adding Conversation Transcript as an update
+                        console.log('new item id', result.data.data.create_item.id)
+                        const newItemId = result.data.data.create_item.id
+                        axios({
+                            url: 'https://api.monday.com/v2',
+                            method: 'post',
+                            data: {
+                                query: `mutation{
                         create_update (item_id: ${newItemId}, body: ${JSON.stringify(conversationTranscript)}) {
                             id
                         }
                     }`
-                },
-                headers: {
-                    'Authorization': MONDAY_AUTH_TOKEN,
-                    'Content-Type': 'application/json'
-                }
-            })
-                .then(res => {
-                    console.log('after create update')
-                    console.log(res)
-                })
-                .catch(err => {
-                    console.log('error', err)
-                })
-        })
-        .catch(err => {
-            console.log(err)
+                            },
+                            headers: {
+                                'Authorization': MONDAY_AUTH_TOKEN,
+                                'Content-Type': 'application/json'
+                            }
+                        })
+                            .then(res => {
+                                console.log('Transcript added succesfully')
+                            })
+                            .catch(err => {
+                                console.log('error', err)
+                            })
+                    })
+                    .catch(err => {
+                        console.log(err)
+                    })
+            }
         })
 }
